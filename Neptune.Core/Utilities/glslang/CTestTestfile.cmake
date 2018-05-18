470db053930e408024ae4088995ac06de56a2613
Aleksey Halahan <ahalahan@gmail.com>
2018-05-18 22:47:33 +0200

    Cleanup after the move to ShaderGen

--- Neptune.Core/Utilities/glslang/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/skyne/ExternalDrive/temp/SPIRV-Cross/external/glslang/StandAlone
-# Build directory: /media/skyne/ExternalDrive/temp/SPIRV-Cross/external/glslang-build/StandAlone
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

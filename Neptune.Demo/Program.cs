b933a2699f17b11bc720113f845e6bcd4c53d3e5
Skyne98 <ahalahan@gmail.com>
2018-06-05 22:27:31 +0200

    Some cleanup

--- Neptune.Demo/Program.cs
+++ Neptune.Demo/Program.cs
@@ -22,134 +22,10 @@ namespace Neptune.Demo
         {
             var createInfo = new EngineWindowCreateInfo()
             {
-                PreferredBackend = GraphicsBackend.OpenGL
+                PreferredBackend = GraphicsBackend.Vulkan
             };
             var myWindow = new MyWindow(createInfo);
             myWindow.Run();
-//            ParallelNative.TransformNative transformNative = new ParallelNative.TransformNative()
-//            {
-//                X = 20,
-//                Y = 30,
-//                Rotation = 45,
-//                SizeX = 2,
-//                SizeY = 3
-//            };
-//            var matrix = Matrix4x4.Identity;
-//            var transforms = new ParallelNative.TransformNative[] {transformNative};
-//            var matrices = new Matrix4x4[] {matrix};
-//            ParallelNative.CalculateMatrices(transforms, matrices);
-//            Console.WriteLine($"Got: {matrices[0]}");
-//            var modelMatrix = Matrix4x4.CreateScale(new Vector3(new Vector2(2, 3), 1.0f));
-//            modelMatrix = modelMatrix * Matrix4x4.CreateTranslation(new Vector3(new Vector2(20, 30), 0));
-//            Console.WriteLine($"Needed: {modelMatrix}");
-//            JobManager jobManager = new JobManager();
-//            List<RecurrentJob> jobs = new List<RecurrentJob>();
-//            for (int i = 0; i < 1000000; i++)
-//            {
-//                var job = new MyJob();
-//                jobs.Add(job);
-//            }
-//
-//            Benchmark(() =>
-//            {
-//                foreach (var job in jobs)
-//                {
-//                    job.Schedule(jobManager);
-//                }
-//                jobManager.WaitAll();
-//            });
-//            foreach (var job in jobs)
-//            {
-//                job.Reset();
-//            }
-//            Benchmark(() =>
-//            {
-//                foreach (var job in jobs)
-//                {
-//                    job.Execute();
-//                }
-//            });
-//            
-//            Benchmark(() =>
-//            {
-//                foreach (var job in jobs)
-//                {
-//                    job.Reset();
-//                }
-//                jobManager.Reset();
-//                foreach (var job in jobs)
-//                {
-//                    job.Schedule(jobManager);
-//                }
-//                jobManager.WaitAll();
-//            });
-//
-//            Benchmark((() =>
-//            {
-//                for (int i = 0; i < 1000000; i++)
-//                {
-//                    var sum = Matrix4x4.Identity;
-//                    for (int x = 0; x < 4; x++)
-//                    {
-//                        sum *= Matrix4x4.CreateRotationX(30); ;
-//                    }
-//                }
-//            }));
-//            var parallelNative = new ParallelNative();
-//
-//            Console.ReadLine();
-        }
-
-        public static void Benchmark(Action action)
-        {
-            var startMemory = GC.GetTotalMemory(true);
-            var startTime = DateTime.Now;
-            action();
-            var resultMemory = GC.GetTotalMemory(false);
-            var resultTime = DateTime.Now;
-            Console.WriteLine($"Time: {(resultTime - startTime).TotalMilliseconds} ms Memory: {resultMemory - startMemory} bytes / {(resultMemory - startMemory) / 1024} kbytes");
-        }
-    }
-
-    public class MyJob : RecurrentJob
-    {
-        public List<RecurrentJob> Dependencies = new List<RecurrentJob>();
-
-        public override List<RecurrentJob> GetDependencies()
-        {
-            return Dependencies;
-        }
-
-        public override void Execute()
-        {
-            var sum = Matrix4x4.Identity;
-            for (int i = 0; i < 4; i++)
-            {
-                sum *= Matrix4x4.CreateRotationX(30);;
-            }
-        }
-    }
-
-    public class MyChildJob : RecurrentJob
-    {
-        private readonly RecurrentJob _parent;
-
-        public MyChildJob(RecurrentJob parent)
-        {
-            _parent = parent;
-        }
-
-        public override List<RecurrentJob> GetDependencies()
-        {
-            return new List<RecurrentJob>()
-            {
-                _parent
-            };
-        }
-
-        public override void Execute()
-        {
-            Console.WriteLine($"Child job executing at {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 
@@ -206,12 +82,6 @@ namespace Neptune.Demo
 
                 ImGui.EndMainMenuBar();
             }
-
-
-            //sprites[0].ZIndex = (float)Math.Max(0d, Math.Cos(loopInfo.GlobalTime));
-            //sprites[0].Rotation += 15f * loopInfo.SecondsPerFrame;
-            //sprites[1].Rotation += 30f * loopInfo.SecondsPerFrame;
-            // Console.Title = Math.Max(0d, Math.Cos(loopInfo.GlobalTime)).ToString();
         }
 
         private unsafe int OnTextEdited(TextEditCallbackData* data)
